c4f57f52f8c47a1c3aedd28f58e41b42a2d1d3b9
Ravichandran <32533558+Ravimoon25@users.noreply.github.com>
2022-08-06 12:58:03 +0530

    Added comments

--- autokey_test.py
+++ autokey_test.py
@@ -1,11 +1,15 @@
+#import package
 import pyautogui as pg
 
+#Add variables for pause gap and failsafe
 pg.PAUSE = 2
 pg.FAILSAFE = True
 
+#Loop for 1000 times
 for i in range(0, 1000):
     pg.moveTo(1300, 100)
     pg.moveTo(1300, 200)
     pg.click()
 
+print("Done")
 
